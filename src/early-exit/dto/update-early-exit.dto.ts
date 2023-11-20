3e0f51aa64d2eef69f6a3667e3a43b64d14936c1
Vinicius Diefenbach <viniciusdiefenbach2004@gmail.com>
2023-11-20 18:53:46 -0300

    feat: c onfigure swagger to update DTOs

--- src/early-exit/dto/update-early-exit.dto.ts
+++ src/early-exit/dto/update-early-exit.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
 import { CreateEarlyExitDto } from './create-early-exit.dto';
 
 export class UpdateEarlyExitDto extends PartialType(CreateEarlyExitDto) {}
