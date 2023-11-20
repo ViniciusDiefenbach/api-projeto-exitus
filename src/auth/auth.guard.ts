eee0d7d36d7f5b06537f32dd8e4b9bf1cce8ea6c
Vinicius Diefenbach <viniciusdiefenbach2004@gmail.com>
2023-11-19 23:17:15 -0300

    fix: change the auth errors name

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,4 +1,11 @@
-import { BadRequestException, CanActivate, ExecutionContext, Injectable, SetMetadata, UnauthorizedException } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  InternalServerErrorException,
+  SetMetadata,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { JwtService } from '@nestjs/jwt';
 import { RoleType } from '@prisma/client';
@@ -10,39 +17,39 @@ export const Roles = (...roles: RoleType[]) => SetMetadata(ROLES_KEY, roles);
 export class AuthGuard implements CanActivate {
   constructor(
     private readonly jwtService: JwtService,
-    private readonly reflector: Reflector
-  ) { }
+    private readonly reflector: Reflector,
+  ) {}
 
-  canActivate(
-    context: ExecutionContext,
-  ): boolean {
-    const roles = this.reflector.get<RoleType[]>(ROLES_KEY, context.getHandler());
-    if (!roles) {
-      return true;
-    }
+  canActivate(context: ExecutionContext): boolean {
+    const roles = this.reflector.get<RoleType[]>(
+      ROLES_KEY,
+      context.getHandler(),
+    );
     const request = context.switchToHttp().getRequest();
     const authHeader = request.headers.authorization;
     if (!authHeader) {
-      throw new BadRequestException('Token não encontrado!');
+      throw new InternalServerErrorException('Token não encontrado!');
     }
     const [, token] = authHeader.split(' ');
     if (!token) {
-      throw new BadRequestException('Token não encontrado!');
+      throw new InternalServerErrorException('Token não encontrado!');
     }
     try {
       !this.jwtService.verify(token, {
-        secret: "teste123"
-      })
+        secret: 'teste123',
+      });
     } catch (e) {
-      throw new BadRequestException('Token inválido!');
+      throw new InternalServerErrorException('Token inválido!');
     }
-    const user = <{
-      sub: string;
-      email: string;
-      role: RoleType;
-    }>this.jwtService.decode(token);
+    const user = <
+      {
+        sub: string;
+        email: string;
+        role: RoleType;
+      }
+    >this.jwtService.decode(token);
     request.user = user;
-    if (roles.includes(user.role)) {
+    if (!roles || roles.includes(user.role)) {
       return true;
     }
     throw new UnauthorizedException('Usuário não autorizado!');
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,8 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { LoginDto } from './dto/login.dto';
 import { PrismaService } from '@/prisma/prisma.service';
 import { BcryptService } from '@/bcrypt/bcrypt.service';
@@ -9,129 +13,142 @@ import { RefreshTokenDto } from './dto/refresh-token.dto';
 
 @Injectable()
 export class AuthService {
-    constructor(
-        private readonly prismaService: PrismaService,
-        private readonly bcryptService: BcryptService,
-        private readonly jwtService: JwtService
-    ) { }
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly bcryptService: BcryptService,
+    private readonly jwtService: JwtService,
+  ) {}
 
-    async login({ role, email, password }: LoginDto) {
-        const user = await this.prismaService.user.findUnique({
-            select: {
-                id: true,
-                email: true,
-                password: true,
-                roles: {
-                    select: {
-                        role: {
-                            select: {
-                                role_type: true
-                            }
-                        }
-                    }
-                }
+  async login({ role, email, password }: LoginDto) {
+    const user = await this.prismaService.user.findUnique({
+      select: {
+        id: true,
+        email: true,
+        password: true,
+        roles: {
+          select: {
+            role: {
+              select: {
+                role_type: true,
+              },
             },
-            where: {
-                email: email
-            }
-        });
+          },
+        },
+      },
+      where: {
+        email: email,
+      },
+    });
 
-        if (!user) {
-            throw new BadRequestException('Usuário não encontrado!')
-        }
-
-        if (await this.bcryptService.compare({
-            password: password,
-            hash: user.password
-        }) == false) {
-            throw new BadRequestException('Senha incorreta!')
-        }
-
-        let choosedRole;
-
-        if (role) {
-            user.roles.forEach((userRole) => {
-                if (userRole.role.role_type == role) {
-                    choosedRole = role;
-                }
-            })                                      
-            if (!choosedRole) {
-                throw new BadRequestException('O usuário não possui a função escolhida')
-            }
-        } else {
-            if (user.roles.length != 1) {
-                throw new BadRequestException('Usuário sem função definida ou com mais de uma função')
-            }
-            choosedRole = user.roles[0].role.role_type;
-        }
-
-        const access_token = this.jwtService.sign({
-            sub: user.id,
-            role: choosedRole as RoleType,
-        }, {
-            secret: "teste123",
-            expiresIn: '60s'
-        })
-
-        const refresh_token = await this.prismaService.refreshToken.create({
-            data: {
-                id: randomUUID(),
-                user_id: user.id,
-                choosed_role: choosedRole as RoleType,
-                expires_at: new Date(new Date().getTime() + 60 * 1000),
-                created_at: new Date(),
-            }
-        })
-
-        return {
-            access_token,
-            refresh_token
-        }
+    if (!user) {
+      throw new BadRequestException('Email ou senha incorreta!');
     }
 
-    async refreshToken({ refresh_token }: RefreshTokenDto) {
-        const token = await this.prismaService.refreshToken.findUnique({
-            where: {
-                id: refresh_token
-            }
-        })
-
-        if (!token) {
-            throw new BadRequestException('Token inválido!')
-        }
-
-        const access_token = this.jwtService.sign({
-            sub: token.user_id,
-            role: token.choosed_role,
-        }, {
-            secret: "teste123",expiresIn: "120s"
-        })
-
-        if (token.expires_at < new Date()) {
-            await this.prismaService.refreshToken.deleteMany({
-                where: {
-                    user_id: token.user_id
-                }
-            })
-
-            const refresh_token = await this.prismaService.refreshToken.create({
-                data: {
-                    id: randomUUID(),
-                    user_id: token.user_id,
-                    choosed_role: token.choosed_role,
-                    expires_at: new Date(new Date().getTime() + 60 * 1000),
-                    created_at: new Date(),
-                }
-            })
-
-            return {
-                access_token,
-                refresh_token
-            }
-        }
-        
-        return {
-            access_token
-        }
+    if (
+      (await this.bcryptService.compare({
+        password: password,
+        hash: user.password,
+      })) == false
+    ) {
+      throw new BadRequestException('Email ou senha incorreta!');
     }
+
+    let choosedRole;
+
+    if (role) {
+      user.roles.forEach((userRole) => {
+        if (userRole.role.role_type == role) {
+          choosedRole = role;
+        }
+      });
+      if (!choosedRole) {
+        throw new InternalServerErrorException(
+          'O usuário não possui a função escolhida',
+        );
+      }
+    } else {
+      if (user.roles.length != 1) {
+        throw new InternalServerErrorException(
+          'Usuário sem função definida ou com mais de uma função',
+        );
+      }
+      choosedRole = user.roles[0].role.role_type;
+    }
+
+    const access_token = this.jwtService.sign(
+      {
+        sub: user.id,
+        role: choosedRole as RoleType,
+      },
+      {
+        secret: 'teste123',
+        expiresIn: '60s',
+      },
+    );
+
+    const refresh_token = await this.prismaService.refreshToken.create({
+      data: {
+        id: randomUUID(),
+        user_id: user.id,
+        choosed_role: choosedRole as RoleType,
+        expires_at: new Date(new Date().getTime() + 60 * 1000),
+        created_at: new Date(),
+      },
+    });
+
+    return {
+      access_token,
+      refresh_token,
+    };
+  }
+
+  async refreshToken({ refresh_token }: RefreshTokenDto) {
+    const token = await this.prismaService.refreshToken.findUnique({
+      where: {
+        id: refresh_token,
+      },
+    });
+
+    if (!token) {
+      throw new InternalServerErrorException('Token inválido!');
+    }
+
+    const access_token = this.jwtService.sign(
+      {
+        sub: token.user_id,
+        role: token.choosed_role,
+      },
+      {
+        secret: 'teste123',
+        expiresIn: '120s',
+      },
+    );
+
+    if (token.expires_at < new Date()) {
+      await this.prismaService.refreshToken.deleteMany({
+        where: {
+          user_id: token.user_id,
+        },
+      });
+
+      const refresh_token = await this.prismaService.refreshToken.create({
+        data: {
+          id: randomUUID(),
+          user_id: token.user_id,
+          choosed_role: token.choosed_role,
+          expires_at: new Date(new Date().getTime() + 60 * 1000),
+          created_at: new Date(),
+        },
+      });
+
+      return {
+        access_token,
+        refresh_token,
+      };
+    }
+
+    return {
+      access_token,
+    };
+  }
 }
